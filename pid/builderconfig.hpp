32dd9ba6b4caa0e037ca2a2e617db8f246f1b8ca
Patrick Venture <venture@google.com>
2018-06-11 14:29:38 -0700

    pid: zone split out builders
    To enable unit-testing, split the builders into their own
    object files.
    
    Tested: Ran on quanta-q71l board and it behaved as expected.
    
    Change-Id: I92168ec9ae4946d12328e9c0b94a36bb89d0f718
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ pid/builderconfig.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <memory>
+#include <sdbusplus/bus.hpp>
+#include <string>
+#include <unordered_map>
+
+#include "pid/zone.hpp"
+#include "sensors/manager.hpp"
+
+std::unordered_map<int64_t, PIDZone*> BuildZonesFromConfig(
+        const std::string& path,
+        SensorManager& mgr,
+        sdbusplus::bus::bus& modeControlBus);
