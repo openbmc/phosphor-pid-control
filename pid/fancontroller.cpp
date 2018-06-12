83080901359a4a104e57a83c55509ee786826c51
Patrick Venture <venture@google.com>
2018-06-12 13:35:05 -0700

    test: add zone_mock
    Starting with the two methods we need.  The interface will
    likely grow to expand all test coverage.
    
    Change-Id: I86ec97da9427084d676bab6bdf15a1b870a8ff98
    Signed-off-by: Patrick Venture <venture@google.com>

--- pid/fancontroller.cpp
+++ pid/fancontroller.cpp
@@ -22,7 +22,7 @@
 #include "zone.hpp"
 
 std::unique_ptr<PIDController> FanController::CreateFanPid(
-    PIDZone* owner,
+    ZoneInterface* owner,
     const std::string& id,
     std::vector<std::string>& inputs,
     ec::pidinfo initial)
