037fd635ccf46d7362b32c5434dceb104807c860
Patrick Venture <venture@google.com>
2018-06-12 13:35:05 -0700

    test: add zone_mock
    Starting with the two methods we need.  The interface will
    likely grow to expand all test coverage.
    
    Tested: Ran on quanta-q71l board and it behaved as expected.
    
    Change-Id: I86ec97da9427084d676bab6bdf15a1b870a8ff98
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ test/zone_mock.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <gmock/gmock.h>
+#include <string>
+
+#include "pid/zone.hpp"
+
+class ZoneMock : public ZoneInterface
+{
+    public:
+        virtual ~ZoneMock() = default;
+
+        MOCK_METHOD1(getCachedValue, double(const std::string&));
+        MOCK_METHOD1(addRPMSetPoint, void(float));
+        MOCK_CONST_METHOD0(getMaxRPMRequest, float());
+        MOCK_CONST_METHOD0(getFailSafeMode, bool());
+        MOCK_CONST_METHOD0(getFailSafePercent, float());
+        MOCK_METHOD1(getSensor, Sensor*(std::string));
+};
