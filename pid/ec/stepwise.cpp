9e1b9d18db622c18934635d2c3d7cbebe5ecbec2
James Feist <james.feist@linux.intel.com>
2018-08-31 14:07:12 -0700

    Add stepwise controller
    This adds the ability to use stepwise curves alongside
    pid control. This creates a base controller class that
    pidcontroller and stepwise controller inherit from.
    
    Note: Hysteresis to come in follow-on patch
    
    Tested-by:
    Created a stepwise controller and noticed that when it
    crossed a threshold that it contributed to the pwm setting.
    
    Change-Id: I6cf842f80eaccafc905d620970afe91e2092d568
    Signed-off-by: James Feist <james.feist@linux.intel.com>

---
+++ pid/ec/stepwise.cpp
@@ -0,0 +1,43 @@
+/*
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include "stepwise.hpp"
+
+#include <cstddef>
+
+namespace ec
+{
+float stepwise(const ec::StepwiseInfo& info, float input)
+{
+    float value = info.output[0]; // if we are below the lowest
+                                  // point, we set the lowest value
+
+    for (size_t ii = 1; ii < ec::maxStepwisePoints; ii++)
+    {
+        if (info.reading[ii] == 0)
+        {
+            break;
+        }
+        if (info.reading[ii] > input)
+        {
+            break;
+        }
+        value = info.output[ii];
+    }
+
+    return value;
+}
+} // namespace ec
