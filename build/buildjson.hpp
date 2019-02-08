5d1870bf269e3698011b31f59558d1e400483a5a
Patrick Venture <venture@google.com>
2019-02-08 11:47:42 -0800

    add support to build sensors from json
    Add support to build sensors from a json configuration file.
    
    Change-Id: Ic5bcbcd01e085ab0d4efaed314af8dc7e82b0b9d
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ build/buildjson.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "conf.hpp"
+
+#include <map>
+#include <nlohmann/json.hpp>
+#include <string>
+
+using json = nlohmann::json;
+
+/**
+ * Given a json object generated from a configuration file, build the sensor
+ * configuration representation. This expecteds the json configuration to be
+ * valid.
+ *
+ * @param[in] data - the json data
+ * @return a map of sensors.
+ */
+std::map<std::string, struct SensorConfig>
+    buildSensorsFromJson(const json& data);
+
+/** TODO: buildConfigJson will be used to validate the json and return the json
+ * object.
+ */
+void buildConfigJson(const std::string& path);
