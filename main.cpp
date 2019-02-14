c19f5d4de87ea40d99403cf138c5df222a797c3c
Patrick Venture <venture@google.com>
2019-02-14 10:59:47 -0800

    main: add json configuration path
    If swampd is compiled to check for its configuration at run-time, it
    will check one specific path.
    
    Change-Id: I5c37ccbb2229db742c0f43b3d9ee0c368df19479
    Signed-off-by: Patrick Venture <venture@google.com>

--- main.cpp
+++ main.cpp
@@ -52,6 +52,9 @@ extern std::map<int64_t, PIDConf> zoneConfig;
 /* The YAML converted Zone configuration. */
 extern std::map<int64_t, struct ZoneConfig> zoneDetailsConfig;
 
+/** the swampd daemon will check for the existence of this file. */
+constexpr auto jsonConfigurationPath = "/usr/share/swampd/config.json";
+
 int main(int argc, char* argv[])
 {
     int rc = 0;
