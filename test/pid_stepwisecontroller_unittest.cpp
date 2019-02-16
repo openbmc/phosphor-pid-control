dca2d484ee91ee40fc94509d831ef2c5fa792ca0
James Feist <james.feist@linux.intel.com>
2019-02-15 16:14:31 -0800

    DBusConfiguration: Fix scaling
    Revert the removal of the /100 because that caused
    90% to be passed as 90 instead of .9. To counter-act
    this multiply the stepwise controller by 100 so that
    setting 50 is 50%.
    
    Tested-by: Used sensor override to make stepwise jump
    threshold and got desired pwm result.
    
    Change-Id: I629bf0d4b0b3bc77660c09fccae82b1bdac4c578

--- test/pid_stepwisecontroller_unittest.cpp
+++ test/pid_stepwisecontroller_unittest.cpp
@@ -12,6 +12,8 @@
 using ::testing::Return;
 using ::testing::StrEq;
 
+constexpr size_t scale = 100; // values are 10 for 10%
+
 TEST(StepwiseControllerTest, HysteresisTestPositive)
 {
     // Verifies positive hysteresis works as expected
@@ -38,8 +40,8 @@ TEST(StepwiseControllerTest, HysteresisTestPositive)
         .WillOnce(Return(31.0))  // return 40
         .WillOnce(Return(32.0)); // return 60
 
-    EXPECT_CALL(z, addRPMSetPoint(40.0)).Times(2);
-    EXPECT_CALL(z, addRPMSetPoint(60.0)).Times(1);
+    EXPECT_CALL(z, addRPMSetPoint(40.0 * scale)).Times(2);
+    EXPECT_CALL(z, addRPMSetPoint(60.0 * scale)).Times(1);
 
     for (int ii = 0; ii < 3; ii++)
     {
@@ -73,8 +75,8 @@ TEST(StepwiseControllerTest, HysteresisTestNegative)
         .WillOnce(Return(27.0))  // return 60
         .WillOnce(Return(26.0)); // return 40
 
-    EXPECT_CALL(z, addRPMSetPoint(40.0)).Times(1);
-    EXPECT_CALL(z, addRPMSetPoint(60.0)).Times(2);
+    EXPECT_CALL(z, addRPMSetPoint(40.0 * scale)).Times(1);
+    EXPECT_CALL(z, addRPMSetPoint(60.0 * scale)).Times(2);
 
     for (int ii = 0; ii < 3; ii++)
     {
