f7383a0fe539c600785dba35ea7eabd10cd493d8
Hank Liou <Hank.Liou@quantatw.com>
2019-05-21 10:16:44 +0800

    Remove sensor nonexistence protection.

--- sensors/builder.cpp
+++ sensors/builder.cpp
@@ -71,7 +71,6 @@ SensorManager
                     passiveListeningBus, info->type, name, &helper, info);
                 if (ri == nullptr)
                 {
-                    system("pwm_tacho.py -a -d 100");
                     throw SensorBuildException(
                         "Failed to create dbus passive sensor: " + name +
                         " of type: " + info->type);
