d607f9958c8f1cbd7381810cdacd82eb2c09974f
Patrick Venture <venture@google.com>
2020-08-11 08:48:19 -0700

    s/PIDZone/DbusPidZone/g
    Renamed PIDZone to DbusPidZone because this object builds in via
    inheritance a Dbus implementation of the Mode control interface.
    
    Signed-off-by: Patrick Venture <venture@google.com>
    Change-Id: Ifc6c11db4952de5909f0e556c41ec25eee217408

--- pid/pidloop.hpp
+++ pid/pidloop.hpp
@@ -12,12 +12,12 @@ namespace pid_control
  * This function calls itself indefinitely in an async loop to calculate
  * fan outputs based on thermal inputs.
  *
- * @param[in] zone - ptr to the PIDZone for this loop.
+ * @param[in] zone - ptr to the DbusPidZone for this loop.
  * @param[in] timer - boost timer used for async callback.
  * @param[in] first - boolean to denote if initialization needs to be run.
  * @param[in] ms100cnt - loop timer counter.
  */
-void pidControlLoop(PIDZone* zone, boost::asio::steady_timer& timer,
+void pidControlLoop(DbusPidZone* zone, boost::asio::steady_timer& timer,
                     bool first = true, int ms100cnt = 0);
 
 } // namespace pid_control
