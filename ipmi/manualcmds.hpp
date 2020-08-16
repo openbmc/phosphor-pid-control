033a65e7bed698f752d1a2863153afccd7a51ec3
Patrick Venture <venture@google.com>
2020-08-16 12:22:54 -0700

    ipmi: add zone controller interface
    Implementations of this interface can make the library mode testable and
    work in environments other than dbus.
    
    The main entry point to the IPMI handler now expects to receive a
    handler pointer.  The handler itself is implemented to process IPMI
    requests and responses in the expected way.  The default object receives
    a dbus implementation for the zone controls.
    
    Signed-off-by: Patrick Venture <venture@google.com>
    Change-Id: I6d7be871bdc5cce3c3cc53c16d80b501aaaafc7d

--- ipmi/manualcmds.hpp
+++ ipmi/manualcmds.hpp
@@ -1,16 +1,42 @@
 #pragma once
 
+#include "control.hpp"
+
 #include <ipmid/api.h>
 
 #include <cstdint>
+#include <memory>
 
 namespace pid_control
 {
 namespace ipmi
 {
 
-ipmi_ret_t manualModeControl(ipmi_cmd_t cmd, const uint8_t* reqBuf,
-                             uint8_t* replyCmdBuf, size_t* dataLen);
+// Implements validation of IPMI commands and handles sending back the
+// responses.
+class ZoneControlIpmiHandler
+{
+  public:
+    ZoneControlIpmiHandler(std::unique_ptr<ZoneControlInterface> control) :
+        _control(std::move(control))
+    {}
+
+    ipmi_ret_t getFailsafeModeState(const uint8_t* reqBuf, uint8_t* replyBuf,
+                                    size_t* dataLen);
+
+    ipmi_ret_t getManualModeState(const uint8_t* reqBuf, uint8_t* replyBuf,
+                                  size_t* dataLen);
+
+    ipmi_ret_t setManualModeState(const uint8_t* reqBuf, uint8_t* replyBuf,
+                                  size_t* dataLen);
+
+  private:
+    std::unique_ptr<ZoneControlInterface> _control;
+};
+
+ipmi_ret_t manualModeControl(ZoneControlIpmiHandler* handler, ipmi_cmd_t cmd,
+                             const uint8_t* reqBuf, uint8_t* replyCmdBuf,
+                             size_t* dataLen);
 
 } // namespace ipmi
 } // namespace pid_control
