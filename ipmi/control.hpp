7c4a6e8e405171ac61725cadf1799b9da63c3f6d
Patrick Venture <venture@google.com>
2020-08-16 12:22:54 -0700

    ipmi: add zone controller interface
    Implementations of this interface can make the library mode testable and
    work in environments other than dbus.
    
    The main entry point to the IPMI handler now expects to receive a
    handler pointer.  The handler itself is implemented to process IPMI
    requests and responses in the expected way.  The default object receives
    a dbus implementation for the zone controls.
    
    Signed-off-by: Patrick Venture <venture@google.com>
    Change-Id: I6d7be871bdc5cce3c3cc53c16d80b501aaaafc7d

---
+++ ipmi/control.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace pid_control
+{
+namespace ipmi
+{
+
+// Implement this interface to control a zone's mode or read back its status.
+class ZoneControlInterface
+{
+  public:
+    // Reads the fan control property (either manual or failsafe) and returns an
+    // IPMI code based on success or failure of this.
+    virtual uint8_t getFanCtrlProperty(uint8_t zoneId, bool* value,
+                                       const std::string& property) = 0;
+
+    // Sets the fan control property (only manual mode is settable presently)
+    // and returns an IPMI code based on success or failure of this.
+    virtual uint8_t setFanCtrlProperty(uint8_t zoneId, bool value,
+                                       const std::string& property) = 0;
+};
+
+} // namespace ipmi
+} // namespace pid_control
