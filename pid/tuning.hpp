b4e2782b7ff095eeb0aa90bd92452d2b7edd5093
Josh Lehan <krellan@google.com>
2020-11-11 00:35:58 -0800

    ThermalController: Adding flag to negate worst-margin input
    The PID math can become easier,
    if the input to the margin PID loop is negated.
    
    In other words, flip the sign of the computed worst-margin value,
    before passing it as input to the margin PID loop.
    
    The setpoint is also correspondingly negated,
    so that it can still properly be compared with the input.
    
    This has the beneficial effect of having greater (more positive)
    margins represent hotter temperatures, which should correspond
    to greater (more positive) fan RPM speeds.
    
    In other words, both numbers will move in the same direction,
    making it easier to work with the PID coefficients,
    as the math will then be similar to a temperature PID loop.
    
    This is optional, to avoid breaking existing behavior.
    To enable, use "--negate" on command line,
    or create "/etc/thermal.d/negate" file (content does not matter).
    
    Signed-off-by: Josh Lehan <krellan@google.com>
    Change-Id: I1a537d6dbfcabf067c1714e4d2244ced2f6c5886

--- pid/tuning.hpp
+++ pid/tuning.hpp
@@ -6,8 +6,17 @@
  * during this run.
  */
 extern bool tuningEnabled;
+
 /** String variable with the folder for writing logs if logging is enabled.
  */
 extern std::string loggingPath;
 /** Boolean variable whether loggingPath is non-empty. */
 extern bool loggingEnabled;
+
+/** Boolean variable enabling negation of the worst-margin input into
+ * the margin PID loop, useful for making the PID math easier:
+ * greater negated margins represent hotter temperatures,
+ * which should correspond to greater fan RPM speeds.
+ * In other words, both numbers will move in the same direction.
+ */
+extern bool negateEnabled;
