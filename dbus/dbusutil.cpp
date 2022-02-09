b7c1007e3bc17e41cdb0c3ae2edba6a9b99e6dcc
Harvey.Wu <Harvey.Wu@quantatw.com>
2022-02-09 10:24:30 +0800

    Using readPath dbuspath in getMatch function
    - If the "name" setting is not same as dbus sensor name,
      it has problem when getMatch function.
    
    For example:
    if json setting like below,
    {
        "name": "fan00",
        "type": "fan",
        "readPath": "/xyz/openbmc_project/sensors/fan_tach/fan0_tach",
        "writePath": "/xyz/openbmc_project/sensors/fan_tach/fan0_pwm",
        "min": 0,
        "max": 255
    }
    
    the swampd will start failed, log like below,
    swampd[17273]: Sensor: fan00 fan /xyz/openbmc_project/sensors/fan_tach/fan0_tach /xyz/openbmc_project/sensors/fan_tach/fan0_pwm
    swampd[17273]: ObjectMapper call failure
    
    Signed-off-by: Harvey.Wu <Harvey.Wu@quantatw.com>
    Change-Id: I08cd374ea7447148e4aeff7b06b288957260cbd2

--- dbus/dbusutil.cpp
+++ dbus/dbusutil.cpp
@@ -111,13 +111,12 @@ std::string getSensorPath(const std::string& type, const std::string& id)
     return std::string("/xyz/openbmc_project/sensors/" + layer + "/" + id);
 }
 
-std::string getMatch(const std::string& type, const std::string& id)
+std::string getMatch(const std::string& path)
 {
     return std::string("type='signal',"
                        "interface='org.freedesktop.DBus.Properties',"
                        "member='PropertiesChanged',"
-                       "path='" +
-                       getSensorPath(type, id) + "'");
+                       "path='" + path + "'");
 }
 
 bool validType(const std::string& type)
