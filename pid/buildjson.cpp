fb766c73a4694eb495177da7a188d3c568840cf3
Bonnie Lo <Bonnie_Lo@wiwynn.com>
2022-10-19 11:15:34 +0800

    Support new methods to calculate PWM output
    Description:
    1. Add one property in fan table that could be used to enable
       accumulation of output PWM of different controllers with same sensor.
    
    2. Adding compile flag in bbappend to select another method to
       calculate PID output.
    
    Design:
    1. Search "accumulateSetPoint" field in fan table.
       If the value was true, accumulate the output PWM of
       different controllers with same profile name.
    
    2. Support two method to calculate PID output that could be chosen by
       adding the following compile flag in bbappend.
    
    EXTRA_OECONF:append = " --enable-check-hysteresis-using-setpoint=yes"
    
    Test Case:
    1. Check the output PWM of different controllers with same profile
       name could be accumulated - pass.
    
    2. Add compile flag and check PID output would not be calculated if
       the input temperature was lower than setpoint - pass.
    
    Please see more details in gist:
    https://gist.github.com/DelphineCCChiu/a6170d3e1a12fc4ee76fad324382fba3
    
    Change-Id: I9f38f250d72545784c6c11be2fde7d45f0b385c4
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>

--- pid/buildjson.cpp
+++ pid/buildjson.cpp
@@ -191,6 +191,12 @@ std::pair<std::map<int64_t, conf::PIDConf>, std::map<int64_t, conf::ZoneConfig>>
         }
         thisZoneConfig.cycleTime.updateThermalsTimeMS = updateCount;
 
+        auto findAccSetPoint = zone.find("accumulateSetPoint");
+        if (findAccSetPoint != zone.end())
+        {
+            findAccSetPoint->get_to(thisZoneConfig.accumulateSetPoint);
+        }
+
         auto pids = zone["pids"];
         for (const auto& pid : pids)
         {
