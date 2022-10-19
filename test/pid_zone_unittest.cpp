09b9183b6e73ee2bf53762dd15cc855b49a4b660
Bonnie Lo <Bonnie_Lo@wiwynn.com>
2022-10-19 11:15:34 +0800

    Support to accumulate controllers' output PWM and don't calculate PID output if input temperature was lower than setpoint
    Description:
    1. Add one property, accumulateSetPoint, in fan table that could be used to
    enable to accumulate the output PWM of different controllers with same sensors.
    
    2. Don't calculate PID output if input temperature was lower than setpoint.
    
    Design:
    1. Search accumulateSetPoint filed in fan table, if the value was true,
    accumulate the output PWM of different contorllers with same profile name.
    
    e.g. If we have two controllers named "Stepwise_Temp_CPU0" and "PID_Temp_CPU0" in fan table,
    their profile name would be "Temp_CPU0".
    
    2. Don't calculate PID output if input temperature was lower than setpoint
    and reinitialize integral term to prevent overshoot.
    
    Test Case:
    1. Check the output PWM of different controllers with same profile name could be accumulated - pass.
    
    2. Check PID output would not be calculated if the input temperature was lower than setpoint - pass.
    
    Change-Id: I9f38f250d72545784c6c11be2fde7d45f0b385c4
    Signed-off-by: Bonnie Lo <Bonnie_Lo@wiwynn.com>

--- test/pid_zone_unittest.cpp
+++ test/pid_zone_unittest.cpp
@@ -46,6 +46,7 @@ TEST(PidZoneConstructorTest, BoringConstructorTest)
     SensorManager m(bus_mock_passive, bus_mock_host);
 
     bool defer = true;
+    bool accSetPoint = false;
     const char* objPath = "/path/";
     int64_t zone = 1;
     double minThermalOutput = 1000.0;
@@ -57,7 +58,7 @@ TEST(PidZoneConstructorTest, BoringConstructorTest)
                     &d);
 
     DbusPidZone p(zone, minThermalOutput, failSafePercent, m, bus_mock_mode,
-                  objPath, defer);
+                  objPath, defer, accSetPoint);
     // Success.
 }
 
@@ -86,9 +87,9 @@ class PidZoneTest : public ::testing::Test
         SetupDbusObject(&sdbus_mock_mode, defer, objPath, modeInterface,
                         properties, &property_index);
 
-        zone = std::make_unique<DbusPidZone>(zoneId, minThermalOutput,
-                                             failSafePercent, mgr,
-                                             bus_mock_mode, objPath, defer);
+        zone = std::make_unique<DbusPidZone>(
+            zoneId, minThermalOutput, failSafePercent, mgr, bus_mock_mode,
+            objPath, defer, accSetPoint);
     }
 
     // unused
@@ -102,6 +103,7 @@ class PidZoneTest : public ::testing::Test
     double minThermalOutput = 1000.0;
     double failSafePercent = 0.75;
     bool defer = true;
+    bool accSetPoint = false;
     const char* objPath = "/path/";
     SensorManager mgr;
 
