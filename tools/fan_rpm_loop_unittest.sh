1daacca202256e9150fa7fda23276b874ba65410
Patrick Williams <patrick@stwcx.xyz>
2022-12-08 06:23:37 -0600

    beautysh: re-format
    beautysh is enabled in the openbmc-build-scripts on Bash/Zsh/POSIX-sh
    files to have a consistent formatting.  Re-run the formatter on the
    whole repository.
    
    Change-Id: Ie7df79b3878cbbc3893ab66ce30ba73c2b200da5
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- tools/fan_rpm_loop_unittest.sh
+++ tools/fan_rpm_loop_unittest.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
 
-SourceModule() {
-  # shellcheck source=tools/fan_rpm_loop_test.sh
-  . fan_rpm_loop_test.sh
+function SourceModule() {
+    # shellcheck source=tools/fan_rpm_loop_test.sh
+    . fan_rpm_loop_test.sh
 }
 
-SetupShims() {
-  MkDir()      { echo "MkDir      $*"; }
-  Mv()         { echo "Mv         $*"; }
-  Sleep()      { echo "Sleep      $*"; }
-  SystemCtl()  { echo "SystemCtl  $*"; }
-  CommandRpm() { echo "CommandRpm $*"; }
+function SetupShims() {
+    function MkDir() { echo "MkDir      $*"; }
+    function Mv() { echo "Mv         $*"; }
+    function Sleep() { echo "Sleep      $*"; }
+    function SystemCtl() { echo "SystemCtl  $*"; }
+    function CommandRpm() { echo "CommandRpm $*"; }
 }
 
-TestRunRpmStepsWorks() {
-  RunRpmSteps 1000 5000 3 30 || return
-  RunRpmSteps 5000 1000 3 30 || return
-  RunRpmSteps 1000 5000 1 30 || return
-  RunRpmSteps 5000 1000 1 30 || return
+function TestRunRpmStepsWorks() {
+    RunRpmSteps 1000 5000 3 30 || return
+    RunRpmSteps 5000 1000 3 30 || return
+    RunRpmSteps 1000 5000 1 30 || return
+    RunRpmSteps 5000 1000 1 30 || return
 }
 
-TestMainRejectsLowMinAndMax() {
-  if main 0 0; then
-    echo "main 0 0 not rejected?"
-    return 1
-  fi
-  if main 1 0; then
-    echo "main 1 0 not rejected?"
-    return 1
-  fi
+function TestMainRejectsLowMinAndMax() {
+    if main 0 0; then
+        echo "main 0 0 not rejected?"
+        return 1
+    fi
+    if main 1 0; then
+        echo "main 1 0 not rejected?"
+        return 1
+    fi
 }
 
-TestMainWorks() {
-  main 1000 5005 || return
+function TestMainWorks() {
+    main 1000 5005 || return
 }
 
-main() {
-  SourceModule                || return
-  SetupShims                  || return
-  TestRunRpmStepsWorks        || return
-  TestMainRejectsLowMinAndMax || return
-  TestMainWorks               || return
-  echo "All tests completed."
+function main() {
+    SourceModule                || return
+    SetupShims                  || return
+    TestRunRpmStepsWorks        || return
+    TestMainRejectsLowMinAndMax || return
+    TestMainWorks               || return
+    echo "All tests completed."
 }
 
 return 0 2>/dev/null
