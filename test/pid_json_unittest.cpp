62df653a1f85c461371272dc83d2ea766d91b752
Josh Lehan <krellan@google.com>
2023-01-13 11:06:16 -0800

    Implementing the TempToMargin feature
    Wrapping the input name std::string in a new structure SensorInput, so
    that the TempToMargin information can be cleanly carried along with
    it, all the way down to the PID input processing layer where it is
    needed. This allows the conversion to be done just-in-time before the
    temperature reading is interpreted, minimizing the blast radius of
    this change. Nonetheless, because of the type change, there was a
    somewhat large blast radius to implement this feature.
    
    The design, and the documentation, is already here:
    https://github.com/openbmc/phosphor-pid-control/issues/23
    
    Tested: Added unit tests for JSON parsing and for proper execution
    of the TempToMargin feature. They pass. Ran it locally, on our
    appropriately-configured system, and it seems to work for me.
    
    Change-Id: I598ba485195aaa70c26e91a1da3ab88fff8c3a4c
    Signed-off-by: Josh Lehan <krellan@google.com>

--- test/pid_json_unittest.cpp
+++ test/pid_json_unittest.cpp
@@ -73,6 +73,71 @@ TEST(ZoneFromJson, oneZoneOnePid)
     EXPECT_DOUBLE_EQ(zoneConfig[1].minThermalOutput, 3000.0);
 }
 
+TEST(ZoneFromJson, marginZone)
+{
+    // Parse a valid configuration with one zone and one PID.
+    // This is a margin zone, and has both kinds of temperature
+    // sensors in it, absolute temperature and margin temperature.
+    // Tests that TempToMargin is parsed correctly.
+
+    std::map<int64_t, conf::PIDConf> pidConfig;
+    std::map<int64_t, conf::ZoneConfig> zoneConfig;
+
+    auto j2 = R"(
+      {
+        "zones" : [{
+          "id": 1,
+          "minThermalOutput": 3000.0,
+          "failsafePercent": 75.0,
+          "pids": [{
+            "name": "myPid",
+            "type": "margin",
+            "inputs": ["absolute0", "absolute1", "margin0", "margin1"],
+            "tempToMargin": [
+              85.0,
+              100.0
+            ],
+            "setpoint": 10.0,
+            "pid": {
+              "samplePeriod": 0.1,
+              "proportionalCoeff": 0.0,
+              "integralCoeff": 0.0,
+              "feedFwdOffsetCoeff": 0.0,
+              "feedFwdGainCoeff": 0.010,
+              "integralLimit_min": 0.0,
+              "integralLimit_max": 0.0,
+              "outLim_min": 30.0,
+              "outLim_max": 100.0,
+              "slewNeg": 0.0,
+              "slewPos": 0.0
+            }
+          }]
+        }]
+      }
+    )"_json;
+
+    std::tie(pidConfig, zoneConfig) = buildPIDsFromJson(j2);
+    EXPECT_EQ(pidConfig.size(), static_cast<u_int64_t>(1));
+    EXPECT_EQ(zoneConfig.size(), static_cast<u_int64_t>(1));
+
+    EXPECT_EQ(pidConfig[1]["myPid"].type, "margin");
+    EXPECT_DOUBLE_EQ(zoneConfig[1].minThermalOutput, 3000.0);
+
+    EXPECT_EQ(pidConfig[1]["myPid"].inputs[0].name, "absolute0");
+    EXPECT_DOUBLE_EQ(pidConfig[1]["myPid"].inputs[0].convertMarginZero, 85.0);
+    EXPECT_EQ(pidConfig[1]["myPid"].inputs[0].convertTempToMargin, true);
+
+    EXPECT_EQ(pidConfig[1]["myPid"].inputs[1].name, "absolute1");
+    EXPECT_DOUBLE_EQ(pidConfig[1]["myPid"].inputs[1].convertMarginZero, 100.0);
+    EXPECT_EQ(pidConfig[1]["myPid"].inputs[1].convertTempToMargin, true);
+
+    EXPECT_EQ(pidConfig[1]["myPid"].inputs[2].name, "margin0");
+    EXPECT_EQ(pidConfig[1]["myPid"].inputs[2].convertTempToMargin, false);
+
+    EXPECT_EQ(pidConfig[1]["myPid"].inputs[3].name, "margin1");
+    EXPECT_EQ(pidConfig[1]["myPid"].inputs[3].convertTempToMargin, false);
+}
+
 TEST(ZoneFromJson, oneZoneOnePidWithHysteresis)
 {
     // Parse a valid configuration with one zone and one PID and the PID uses
