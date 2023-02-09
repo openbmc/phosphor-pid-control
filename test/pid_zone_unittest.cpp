b7db07445ee436cca7d19763cf1549d37c5ab817
Harvey Wu <Harvey.Wu@quantatw.com>
2023-02-09 14:58:55 +0800

    Add new property Leader to .State.FanCtrl
    - Adding new property Leader to new interface
      xyz.openbmc_project.Control.Zone to display the driving thermal PID
      which calculate the highest fan RPM setpoint for this zone.
    
    TEST:
    busctl introspect xyz.openbmc_project.State.FanCtrl
                      /xyz/openbmc_project/settings/fanctrl/zone0
                      xyz.openbmc_project.Control.Zone
    NAME              TYPE      SIGNATURE RESULT/VALUE FLAGS
    .Leader           property  s         "CPU0 PID"   emits-change
    
    Signed-off-by: Harvey Wu <Harvey.Wu@quantatw.com>
    Change-Id: I6fcfa596eec6b51a7727c2a01e7d36e3698eebcf

--- test/pid_zone_unittest.cpp
+++ test/pid_zone_unittest.cpp
@@ -34,6 +34,11 @@ TEST(PidZoneConstructorTest, BoringConstructorTest)
 {
     // Build a PID Zone.
 
+    boost::asio::io_context io;
+    auto system_bus = std::make_shared<sdbusplus::asio::connection>(
+        io, sdbusplus::bus_t::session);
+    sdbusplus::asio::object_server object_server(system_bus);
+
     sdbusplus::SdBusMock sdbus_mock_passive, sdbus_mock_host, sdbus_mock_mode;
     auto bus_mock_passive = sdbusplus::get_mocked_new(&sdbus_mock_passive);
     auto bus_mock_host = sdbusplus::get_mocked_new(&sdbus_mock_host);
@@ -59,7 +64,7 @@ TEST(PidZoneConstructorTest, BoringConstructorTest)
                     &d);
 
     DbusPidZone p(zone, minThermalOutput, failSafePercent, cycleTime, m,
-                  bus_mock_mode, objPath, defer);
+                  bus_mock_mode, object_server, objPath, defer);
     // Success.
 }
 
@@ -72,6 +77,11 @@ class PidZoneTest : public ::testing::Test
         property_index(), properties(), sdbus_mock_passive(), sdbus_mock_host(),
         sdbus_mock_mode()
     {
+        boost::asio::io_context io;
+        auto system_bus = std::make_shared<sdbusplus::asio::connection>(
+            io, sdbusplus::bus_t::session);
+        sdbusplus::asio::object_server object_server(system_bus);
+
         EXPECT_CALL(sdbus_mock_host,
                     sd_bus_add_object_manager(
                         IsNull(), _, StrEq("/xyz/openbmc_project/extsensors")))
@@ -88,9 +98,9 @@ class PidZoneTest : public ::testing::Test
         SetupDbusObject(&sdbus_mock_mode, defer, objPath, modeInterface,
                         properties, &property_index);
 
-        zone = std::make_unique<DbusPidZone>(zoneId, minThermalOutput,
-                                             failSafePercent, cycleTime, mgr,
-                                             bus_mock_mode, objPath, defer);
+        zone = std::make_unique<DbusPidZone>(
+            zoneId, minThermalOutput, failSafePercent, cycleTime, mgr,
+            bus_mock_mode, object_server, objPath, defer);
     }
 
     // unused
