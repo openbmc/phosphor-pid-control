123aa921ebaf7637fcaa746402b94b0e1e6b3218
Timothy Pearson <tpearson@raptorengineering.com>
2023-06-07 23:24:14 -0500

    Add standard PID algorithm for SIE and Raptor systems
    Algorithm type defaults to the Google algorithm, but
    can be changed in the JSON configuration file.
    
    Change-Id: I23abda817507802b59a033e1a7350c7118a2ed63
    Signed-off-by: Timothy Pearson <tpearson@raptorengineering.com>

---
+++ pid/ec/pid_common.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace pid_control
+{
+namespace ec
+{
+
+enum class PIDControlType
+{
+    GOOGLE,
+    PID,
+};
+
+typedef struct
+{
+    double min;
+    double max;
+} limits_t;
+
+/* Note: If you update these structs you need to update the copy code in
+ * pid/util.cpp.
+ */
+typedef struct
+{
+    bool initialized;           // has pid been initialized
+    PIDControlType algorithmId; // algorithm type
+
+    double ts;                  // sample time in seconds
+    double integral;            // intergal of error
+    double lastOutput;          // value of last output
+    double lastError;           // value of last error
+
+    double proportionalCoeff;   // coeff for P
+    double integralCoeff;       // coeff for I
+    double derivativeCoeff;     // coeff for D
+    double feedFwdOffset;       // offset coeff for feed-forward term
+    double feedFwdGain;         // gain for feed-forward term
+
+    limits_t integralLimit;     // clamp of integral
+    limits_t outLim;            // clamp of output
+    double slewNeg;
+    double slewPos;
+    double positiveHysteresis;
+    double negativeHysteresis;
+} pid_info_t;
+
+/* Condensed version for use by the configuration. */
+struct pidinfo
+{
+    PIDControlType algorithmId; // algorithm type
+    double ts;                  // sample time in seconds
+    double proportionalCoeff;   // coeff for P
+    double integralCoeff;       // coeff for I
+    double derivativeCoeff;     // coeff for D
+    double feedFwdOffset;       // offset coeff for feed-forward term
+    double feedFwdGain;         // gain for feed-forward term
+    ec::limits_t integralLimit; // clamp of integral
+    ec::limits_t outLim;        // clamp of output
+    double slewNeg;
+    double slewPos;
+    double positiveHysteresis;
+    double negativeHysteresis;
+};
+
+} // namespace ec
+} // namespace pid_control
---
+++ pid/ec/pid_standard.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+// For pid_info_t struct
+#include "pid_common.h"
+
+namespace pid_control
+{
+namespace ec
+{
+
+double pid_standard(pid_info_t* pidinfoptr, double input, double setpoint,
+                    const std::string* nameptr = nullptr);
+
+} // namespace ec
+} // namespace pid_control
