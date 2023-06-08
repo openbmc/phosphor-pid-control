1e8b86c7398052352ed31ccff0475939a450b8d0
Timothy Pearson <tpearson@raptorengineering.com>
2023-06-07 23:24:14 -0500

    Add standard PID algorithm for SIE and Raptor systems
    Algorithm type defaults to the Google algorithm, but
    can be changed in the JSON configuration file.
    
    Change-Id: I23abda817507802b59a033e1a7350c7118a2ed63
    Signed-off-by: Timothy Pearson <tpearson@raptorengineering.com>

--- pid/ec/pid.hpp
+++ pid/ec/pid.hpp
@@ -8,6 +8,12 @@ namespace pid_control
 namespace ec
 {
 
+enum class PIDControlType
+{
+    GOOGLE,
+    PID,
+};
+
 typedef struct
 {
     double min;
@@ -19,21 +25,22 @@ typedef struct
  */
 typedef struct
 {
-    bool initialized;         // has pid been initialized
+    bool initialized;           // has pid been initialized
+    PIDControlType algorithmId; // algorithm type
 
-    double ts;                // sample time in seconds
-    double integral;          // intergal of error
-    double lastOutput;        // value of last output
-    double lastError;         // value of last error
+    double ts;                  // sample time in seconds
+    double integral;            // intergal of error
+    double lastOutput;          // value of last output
+    double lastError;           // value of last error
 
-    double proportionalCoeff; // coeff for P
-    double integralCoeff;     // coeff for I
-    double derivativeCoeff;   // coeff for D
-    double feedFwdOffset;     // offset coeff for feed-forward term
-    double feedFwdGain;       // gain for feed-forward term
+    double proportionalCoeff;   // coeff for P
+    double integralCoeff;       // coeff for I
+    double derivativeCoeff;     // coeff for D
+    double feedFwdOffset;       // offset coeff for feed-forward term
+    double feedFwdGain;         // gain for feed-forward term
 
-    limits_t integralLimit;   // clamp of integral
-    limits_t outLim;          // clamp of output
+    limits_t integralLimit;     // clamp of integral
+    limits_t outLim;            // clamp of output
     double slewNeg;
     double slewPos;
     double positiveHysteresis;
@@ -46,6 +53,7 @@ double pid(pid_info_t* pidinfoptr, double input, double setpoint,
 /* Condensed version for use by the configuration. */
 struct pidinfo
 {
+    PIDControlType algorithmId; // algorithm type
     double ts;                  // sample time in seconds
     double proportionalCoeff;   // coeff for P
     double integralCoeff;       // coeff for I
