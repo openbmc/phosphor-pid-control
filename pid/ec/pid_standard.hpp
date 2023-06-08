b91fe59e3968fc8dae50e5b758e86ed7199335d3
Timothy Pearson <tpearson@raptorengineering.com>
2023-06-07 23:24:14 -0500

    Add standard PID algorithm for SIE and Raptor systems
    Algorithm type defaults to the Google algorithm, but
    can be changed in the JSON configuration file.
    
    Change-Id: I23abda817507802b59a033e1a7350c7118a2ed63
    Signed-off-by: Timothy Pearson <tpearson@raptorengineering.com>

---
+++ pid/ec/pid_standard.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstdint>
+
+namespace pid_control
+{
+namespace ec
+{
+
+double pid_standard(pid_info_t* pidinfoptr, double input, double setpoint,
+                    const std::string* nameptr = nullptr);
+
+} // namespace ec
+} // namespace pid_control
