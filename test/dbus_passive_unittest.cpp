7762c8ece91af53148e1ab4f8e6dedf70a4d9568
Eric Yang <eric.yang.wiwynn@gmail.com>
2025-08-26 10:20:01 +0000

    dbuspassive: Fail reason for missing D-Bus object
    When handle-missing-object-paths is enabled and the expected D-Bus
    object path is not exported, the sensor is marked as object-missing and
    getFailReason() reports "Sensor D-Bus object missing".
    
    Motivation:
    Provide clearer diagnostics by distinguishing a missing D-Bus object
    from bad readings.
    
    Change-Id: I49d98b27a49c5c6d4f523e7af9fd7c5b80198b73
    Signed-off-by: Eric Yang <eric.yang.wiwynn@gmail.com>

--- test/dbus_passive_unittest.cpp
+++ test/dbus_passive_unittest.cpp
@@ -64,8 +64,8 @@ TEST(DbusPassiveTest, BoringConstructorTest)
     auto helper = std::make_unique<DbusHelperMock>();
     SensorProperties properties;
 
-    DbusPassive(bus_mock, type, id, std::move(helper), properties, false, path,
-                nullptr);
+    DbusPassive(bus_mock, type, id, std::move(helper), properties, false, false,
+                path, nullptr);
     // Success
 }
 
